6a2045fbafecbe0d5788965cfba12ab3593895b4
Emi <sakura.math.sashimo@gmail.com>
2020-07-26 16:30:02 +0900

    rewritten corpus

--- create-corpus.py
+++ create-corpus.py
@@ -1,26 +1,26 @@
 import json
 
-with open('new-corpus-conala.txt', 'a') as newfile :
-
-    # with open('conala-json/conala-test.json') as fin :
-    #     for data in json.load(fin) :
-    #     # for line in fin :
-    #         # data = json.load(line)
-    #         # load() 辞書型になる
-    #         s = str(data['snippet'])
-    #         i = str(data['intent'])
-    #         ri = str(data['rewritten_intent'])
-    #         l = str('<sos>' + s + '<tab>' + i + '<tab>' + ri + '<eos>')
-    #         newfile.write(l)
-    #         newfile.write('\n')
+with open('corpus-conala-retrain.txt', 'a') as newfile :
 
     with open('conala-json/conala-train.json') as fin :
         for data in json.load(fin) :
         # for line in fin :
             # data = json.load(line)
+            # load() 辞書型になる
             s = str(data['snippet'])
-            i = str(data['intent'])
-            # ri = str(data['rewritten_intent'])
-            l = str('<sos>' + s + '<tab>' + i + '<eos>')
+            # i = str(data['intent'])
+            ri = str(data['rewritten_intent'])
+            l = str('<sos>' + s + '<tab>' + ri + '<eos>')
             newfile.write(l)
             newfile.write('\n')
+
+    # with open('conala-json/conala-train.json') as fin :
+    #     for data in json.load(fin) :
+    #     # for line in fin :
+    #         # data = json.load(line)
+    #         s = str(data['snippet'])
+    #         i = str(data['intent'])
+    #         # ri = str(data['rewritten_intent'])
+    #         l = str('<sos>' + s + '<tab>' + i + '<eos>')
+    #         newfile.write(l)
+    #         newfile.write('\n')
